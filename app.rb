8556351d8e12216f71e28ced4233478cc8ff6fd3
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-14 20:29:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @string = params.inspect
+    @params = params
+    erb :team
+  end
+
 
 end
